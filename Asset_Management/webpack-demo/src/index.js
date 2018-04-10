f108487d165e9f2e20c8874e5898f49d29bbc0d9
may_xys <may_xys@163.com>
2018-04-10 14:52:02 +0800

    管理资源-Loading 图片

--- Asset_Management/webpack-demo/src/index.js
+++ Asset_Management/webpack-demo/src/index.js
@@ -1,11 +1,16 @@
 import _ from 'lodash'
 import './style.css'
+import Icon from './icon.png'
 function component() {
     var element = document.createElement('div');
 
     //通过 import 引入 Lodash
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     element.classList.add('hello');
+    // 将图像添加到我们现有的 div。
+    let myIcon = new Image();
+    myIcon.src = Icon;
+    element.appendChild(myIcon);
     return element;
 }
 
