6fad26776be53ce25628cb4886f3b090a5553b1c
may_xys <may_xys@163.com>
2018-04-10 13:32:27 +0800

    起步-创建一个bundle文件

--- getting-started/webpack-demo/src/index.js
+++ getting-started/webpack-demo/src/index.js
@@ -1,9 +1,8 @@
+import _ from 'lodash'
 function component() {
     var element = document.createElement('div');
 
-    // Lodash, currently included via a script, is required for this line to work
-    //在index.html 中通过<script src="https://unpkg.com/lodash@4.16.6"></script>  隐式依赖lodash
-    //Lodash使用了一个简单的 _ 符号，就像Jquery的 $ 一样，十分简洁。 参考：https://www.jianshu.com/p/d46abfa4ddc9
+    //通过 import 引入 Lodash
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
     return element;
