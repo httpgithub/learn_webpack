921e3438d38782ffcf9364936b71be8d3b5449cd
may_xys <may_xys@163.com>
2018-04-13 10:31:56 +0800

    管理资源-Loading xml

--- Asset_Management/webpack-demo/webpack.config.js
+++ Asset_Management/webpack-demo/webpack.config.js
@@ -6,27 +6,38 @@ module.exports = {
         path: path.resolve(__dirname, 'dist')
     },
     module: {
-         rules: [
-           {
-             test: /\.css$/,
-             use: [
-               'style-loader',
-               'css-loader'
-             ]
-       },
-       {
-                    test: /\.(png|svg|jpg|gif)$/,
-             use: [{
-                   loader:'file-loader',
-                 options:{
-                     //publicPath:"/p/publicPath/",
-                    // outputPath:"o/dist/outputPath/",
-                     //name: '[path][name].[ext]'
-                 }
-             }
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        //publicPath:"/p/publicPath/",
+                        // outputPath:"o/dist/outputPath/",
+                        //name: '[path][name].[ext]'
+                    }
+                }
 
-             ]
-       }
-     ]
-   }
+                ]
+            }, {
+                test: /\.(csv|tsv)$/,
+                use: [
+                    'csv-loader'
+                ]
+            },
+            {
+                test: /\.xml$/,
+                use: [
+                    'xml-loader'
+                ]
+            }
+        ]
+    }
 };
